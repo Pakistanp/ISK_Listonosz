8c707353745b88e4719e20304b13e36f4b35d03b
Pakistanp <ppp16@vp.pl>
2020-03-29 15:19:25 +0200

    Add main class and Graph class

---
+++ src/main/java/ISK_Listonosz/Graph.java
@@ -0,0 +1,11 @@
+package ISK_Listonosz;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Graph {
+
+    public Graph(String filePath) {
+        Path graphFilePath = Paths.get(filePath);
+    }
+}
---
+++ src/main/java/ISK_Listonosz/RouteInspection.java
@@ -0,0 +1,10 @@
+package ISK_Listonosz;
+
+public class RouteInspection {
+
+    public static void main(String [] args)
+    {
+        System.out.println("TEST");
+        
+    }
+}
