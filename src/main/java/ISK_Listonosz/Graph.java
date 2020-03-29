28a0c439e5b228ebc6deeec1c74517be0cd0012a
Pakistanp <ppp16@vp.pl>
2020-03-29 16:16:12 +0200

    Add loaded from file by lines.

--- src/main/java/ISK_Listonosz/Graph.java
+++ src/main/java/ISK_Listonosz/Graph.java
@@ -1,16 +1,23 @@
 package ISK_Listonosz;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Graph {
 
-    public Graph(String filePath) {
+    public Graph(String filePath) throws IOException {
         Path graphFilePath = Paths.get(filePath);
+        try {
+            Files.lines(graphFilePath).forEach(this::LoadEdgesAndCosts);
+        } catch (IOException e) {
+            throw new IllegalStateException("An error when initialising the graph");
+        }
 
     }
 
-    private void LoadEdges() {
+    private void LoadEdgesAndCosts(String line) {
 
     }
 }
