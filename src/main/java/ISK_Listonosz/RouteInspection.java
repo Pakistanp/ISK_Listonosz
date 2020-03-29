6e5e544d1acd141cc943f697725482221c6ec4bc
Pakistanp <ppp16@vp.pl>
2020-03-29 15:28:54 +0200

    Begin file reading

--- src/main/java/ISK_Listonosz/RouteInspection.java
+++ src/main/java/ISK_Listonosz/RouteInspection.java
@@ -2,9 +2,10 @@ package ISK_Listonosz;
 
 public class RouteInspection {
 
+    private final static String PATH = "src/main/resources/graphs/";
     public static void main(String [] args)
     {
         System.out.println("TEST");
-        
+        Graph graphOne = new Graph(PATH + "graphTEST");
     }
 }
